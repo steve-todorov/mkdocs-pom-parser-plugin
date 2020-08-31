ccd009a556ae4628d739134d2efe0b24337ae571
Steve Todorov <steve.todorov@carlspring.com>
2020-08-31 22:29:13 +0300

    Polishing release script.

--- create-release.sh
+++ create-release.sh
@@ -22,10 +22,25 @@ git add setup.py
 git commit -m "Releasing $VERSION"
 git tag "$VERSION"
 
+while true; do
+  read -p "Next developer version: " NEXT_VERSION
+  if [ -z "${RELEASED_VERSIONS##*NEXT_VERSION*}" ]; then
+    echo "This version has already been released! Pick another one";
+  else
+    break;
+  fi
+done
+
+sed -i "s/version='.*',/version='$NEXT_VERSION.dev1',/" setup.py
+git add setup.py
+git commit -m "Set the next development version."
+
+./build.sh
+
 echo $REMOTE
 
 while true; do
-  read -p "Push? [y|n]" PUSH
+  read -p "Push? [y|n] " PUSH
   case $PUSH in
     [Yy]* ) git push -u origin --all && git push -u origin --tags; break;;
     [Nn]* ) exit;;
